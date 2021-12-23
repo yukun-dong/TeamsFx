e23e3d5abbd884085ee311fc66dee55c26cec9e0
pengfeizhao <pengfeizhao@microsoft.com>
2021-12-23 13:29:13 +0800

    chore: fix IT test

--- packages/fx-core/tests/common/deps-checker/adapters/testLogger.ts
+++ packages/fx-core/tests/common/deps-checker/adapters/testLogger.ts
@@ -3,10 +3,12 @@ import { LogLevel } from "@microsoft/teamsfx-api";
 
 export class TestLogger implements DepsLogger {
   public append(message: string): Promise<boolean> {
-    throw new Error("Method not implemented.");
+    this.writeLog(LogLevel.Debug, message);
+    return Promise.resolve(true);
   }
   public appendLine(message: string): Promise<boolean> {
-    throw new Error("Method not implemented.");
+    this.writeLog(LogLevel.Debug, message);
+    return Promise.resolve(true);
   }
   public debug(message: string): Promise<boolean> {
     this.writeLog(LogLevel.Debug, message);
