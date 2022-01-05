de357d42a14f74d06a654eda97efb4ab4c221179
pengfeizhao <pengfeizhao@microsoft.com>
2022-01-05 18:01:25 +0800

    chore: draft extension

--- packages/vscode-extension/src/debug/depsChecker/spfxNodeChecker.ts
+++ packages/vscode-extension/src/debug/depsChecker/spfxNodeChecker.ts
@@ -1,15 +1,15 @@
-import { DepsCheckerEvent, nodeNotFoundHelpLink, nodeNotSupportedForSPFxHelpLink } from "./common";
-import { NodeChecker } from "./nodeChecker";
-
-export class SPFxNodeChecker extends NodeChecker {
-  protected readonly _nodeNotFoundHelpLink = nodeNotFoundHelpLink;
-  protected readonly _nodeNotSupportedEvent = DepsCheckerEvent.nodeNotSupportedForSPFx;
-
-  protected async getNodeNotSupportedHelpLink(): Promise<string> {
-    return nodeNotSupportedForSPFxHelpLink;
-  }
-
-  protected async getSupportedVersions(): Promise<string[]> {
-    return ["10", "12", "14"];
-  }
-}
+// import { DepsCheckerEvent, nodeNotFoundHelpLink, nodeNotSupportedForSPFxHelpLink } from "./common";
+// import { NodeChecker } from "./nodeChecker";
+//
+// export class SPFxNodeChecker extends NodeChecker {
+//   protected readonly _nodeNotFoundHelpLink = nodeNotFoundHelpLink;
+//   protected readonly _nodeNotSupportedEvent = DepsCheckerEvent.nodeNotSupportedForSPFx;
+//
+//   protected async getNodeNotSupportedHelpLink(): Promise<string> {
+//     return nodeNotSupportedForSPFxHelpLink;
+//   }
+//
+//   protected async getSupportedVersions(): Promise<string[]> {
+//     return ["10", "12", "14"];
+//   }
+// }
