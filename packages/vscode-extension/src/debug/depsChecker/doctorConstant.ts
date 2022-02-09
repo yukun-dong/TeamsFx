544f7404d2b7ed765f32f9d8d97e3fde3dc5dd0e
pengfeizhao <pengfeizhao@microsoft.com>
2022-02-09 17:27:53 +0800

    chore: temp

--- packages/vscode-extension/src/debug/depsChecker/doctorConstant.ts
+++ packages/vscode-extension/src/debug/depsChecker/doctorConstant.ts
@@ -14,10 +14,18 @@ export const doctorConstant = {
   Summary: "Prerequisites Check Summary:",
   RestartVSCode:
     "Restart all your Visual Studio Code instances after the installation is finished.",
+
   NodeNotFound: `Cannot find Node.js. ${InstallNode}`,
   NodeNotSupported: `Node.js (@CurrentVersion) is not in the supported version list (@SupportedVersions). ${InstallNode}`,
+  NodeSuccess: `Supported Node.js version (@Version) is installed.`,
+  SignInSuccess: `M365 Account (@account) is logged in and sidelaoding enabled.`,
   SignInCancelled:
     "Sign in cancelled. The Teams Toolkit requires a Microsoft 365 organizational account where Teams is running and has been registered.",
+  Cert: "Development certificate for localhost",
+  CertSuccess: "Devlopment certification for localhost is installed.",
+  NpmInstallSuccess: "NPM Install for {@app} is executed.",
+  NpmInstallFailue: "NPM Install for {@app}.",
+
   HelpLink: `Please refer to @Link for more information.`,
   LearnMore: `Visit @Link to learn more about prerequisites check.`,
 };
